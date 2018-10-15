2fb54b2aae320feb3a233bf8716ac9354a4271b5
Gonzalo Rocha <grochadc@gmail.com>
2018-10-15 12:48:49 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const reddit = require("./reddit.js");
+const tumblr = require("./tumblrClient.js");
+const getImageData = require("./getImageData.js");
+
+reddit().then(links => {
+  links.forEach(link => {
+    let extension = link.url.substr(link.url.length - 3);
+    if (extension === "png" || "jpg" || "peg") {
+      getImageData(link.url)
+        .then(image => {
+          tumblr.post({
+            caption: link.title,
+            data64: image,
+            source: "http://www.reddit.com/" + links[0].permalink
+          });
+        })
+        .catch(err => console.error(err.message));
+    }
+  });
+});
+
+/* getImageData("https://i.imgur.com/YvijplN.jpg")
+  .then(image => console.log(image))
+  .catch(err => console.error("Error:", err.message));
+  */
