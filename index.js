a3884ad71e6891cb59b279d7cc599de5e894f2a5
Gonzalo Rocha <grochadc@gmail.com>
2018-10-23 11:41:40 -0500

    Factor index.js to be async

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const tumblr = require("./clients/tumblrClient.js");
 const getImageData = require("./lib/getImageData.js");
 const path = require("path");
 
-reddit("r/natureisfuckinglit").then(links => {
-  links.forEach((link, index) => {
-    if (path.extname(link.url) == (".jpg" || ".jpeg" || ".png")) {
-      console.log("Processing an image on index: ", index);
-      getImageData(link.url) //returns image in base64
-        .then(image => {
-          tumblr.post({
-            caption: `<a href="${link.url}">${link.title}</a>`,
-            data64: image
-          });
-        })
-        .catch(err => console.error(err.message));
-    }
-  });
-});
+(async () => {
+  try {
+    let links = await reddit("r/natureisfuckinglit");
+    let photoLinks = links.filter(
+      link =>
+        typeof link.url == "string" &&
+        path.extname(link.url) == (".jpg" || ".jpeg" || ".png")
+    );
+    photoLinks.forEach(async link => {
+      let image = await getImageData(link.url);
+      tumblr.post({
+        caption: `<a href="http://reddit.com/${link.permalink}"> ${
+          link.title
+        }</a>`,
+        data64: image
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+})();
