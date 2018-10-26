cb7f99f40ba4ac108c0ec91612af5c47df63989a
Gonzalo Rocha <grochadc@github.com>
2018-10-26 13:43:56 -0500

    Check for duplicates now working

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ const axios = require("axios");
 
 (async () => {
   try {
-    let links = await reddit("r/natureisfuckinglit");
-    let photoLinks = links.filter(
+    let links = (await reddit("r/natureisfuckinglit")).filter(
       link =>
         typeof link.url == "string" &&
         path.extname(link.url) == (".jpg" || ".jpeg" || ".png")
     );
+    console.log("Links ", links.length);
     let posted = (await axios(
       "https://www.jsonstore.io/7400e86e797b508a5c9269d5662cf79b0442acd032f8f40cf4bd44faf1521ef8/posted/"
     )).data.result;
-    let comparedNames = compare(posted, photoLinks.map(link => link.name));
-    let linksToPost = photoLinks.filter(
-      link => comparedNames.indexOf(link.name) <= 0
-    );
-    let posting = [];
-    linksToPost.forEach(async link => {
+    let queue = compare(posted, links);
+    console.log("Queue ", queue.length);
+    queue.forEach(async link => {
       let image = await getImageData(link.url);
       tumblr.post({
         caption: `<a href="http://reddit.com/${link.permalink}"> ${
@@ -29,9 +26,9 @@ const axios = require("axios");
         }</a>`,
         data64: image
       });
-      posting.push(link.name);
     });
-    let merged = posted.concat(posting);
+    let merged = posted.concat(queue.map(link => link.name));
+    console.log("Merged ", merged.length);
     await axios.post(
       "https://www.jsonstore.io/7400e86e797b508a5c9269d5662cf79b0442acd032f8f40cf4bd44faf1521ef8/posted/",
       merged
